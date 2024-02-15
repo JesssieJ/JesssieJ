eed87d42142de8212b14f5c678f2a9227c112fa3
JesssieJ <xinwei.jin@outlook.com>
2024-02-15 23:55:05 +0800

    .

--- functions/transform parquet.py
+++ functions/transform parquet.py
@@ -1,7 +1,7 @@
 import os
 import pandas as pd
-import pyarrow as pa
-import pyarrow.parquet as pq
+import time
+
 
 def convert_csv_to_parquet(source_folder, target_folder):
     """
@@ -9,12 +9,9 @@ def convert_csv_to_parquet(source_folder, target_folder):
     Only converts files that haven't already been converted.
     After conversion, the original CSV file is deleted to save space.
     """
-    # Get a list of all CSV files in the source folder
     csv_files = [f for f in os.listdir(source_folder) if f.endswith('.csv')]
 
-    # Convert each CSV file to Parquet
     for csv_file in csv_files:
-        # Define the source and target file paths
         source_file_path = os.path.join(source_folder, csv_file)
         # parquet_filename = os.path.splitext(csv_file)[0] + '.parquet'
         parquet_filename = csv_file.replace('.csv', '.parquet')
@@ -23,34 +20,29 @@ def convert_csv_to_parquet(source_folder, target_folder):
         # Check if the Parquet file already exists
         if not os.path.exists(target_file_path):
             print(f"Converting {csv_file} to Parquet format.")
-            # Read the CSV file into a DataFrame
-            df = pd.read_csv(source_file_path,sep=';')
-            # Convert the DataFrame to a Parquet file
+            df = pd.read_csv(source_file_path
+                             # ,sep=';'
+                             )
             df.to_parquet(target_file_path)
 
-            # Delete the original CSV file to save space
+            # Delete original CSV file
             os.remove(source_file_path)
             print(f"Deleted original CSV file: {csv_file}")
         # else:
         #     print(f"Parquet file for {csv_file} already exists. Skipping conversion.")
 
-def get_source(target_folder, columns_to_merge):
+def get_source(target_folder, cols_to_merge):
     """
     Reads all Parquet files in the target folder, extracts specific columns, and merges them into a single DataFrame.
     """
-    # Get a list of all Parquet files in the target folder
     parquet_files = [f for f in os.listdir(target_folder) if f.endswith('.parquet')]
-
-    # Initialize an empty DataFrame for the final merged data
     dfs = []
 
-    # Read each Parquet file and concatenate the specific columns
     for parquet_file in parquet_files:
         file_path = os.path.join(target_folder, parquet_file)
-        df = pd.read_parquet(file_path, columns=columns_to_merge)
+        df = pd.read_parquet(file_path, columns=cols_to_merge)
         dfs.append(df)
 
-    # Concatenate all DataFrames from the list
     merged_df = pd.concat(dfs, ignore_index=True)
 
     return merged_df
@@ -58,4 +50,21 @@ def get_source(target_folder, columns_to_merge):
 source_folder = r'/Users/jessie/Desktop/test/source'
 target_folder = r'/Users/jessie/Desktop/test/parquet'
 
-convert_csv_to_parquet(source_folder, target_folder)
+
+if __name__ == '__main__':
+
+    start_time = time.time()
+    convert_csv_to_parquet(source_folder, target_folder)
+    end_time = time.time()
+    run_time = end_time - start_time
+    print(run_time)
+
+    cols = ['date_received', 'product', 'sub_product']
+    start_time = time.time()
+    a = get_source(target_folder, cols)
+    end_time = time.time()
+    run_time = end_time - start_time
+    print(run_time)
+
+
+
